4a42d35a659880e111a0db3cf6c4f4717d8c07d3
Vijaya150 <vijayadarshini1503@gmail.com>
2024-10-25 00:16:18 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,12 @@
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+}
 # Define AWS region variable
 variable "aws_region" {
   description = "AWS region where the infrastructure will be deployed"
