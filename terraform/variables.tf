a28894b9287df505b865447d964e6cb62d7bf5f3
Vijaya150 <vijayadarshini1503@gmail.com>
2024-10-25 22:52:31 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,26 +1,52 @@
-variable "aws_access_key_id" {
-  description = "AWS access key ID"
+# variables.tf
+
+# Define the AWS region for resource deployment
+variable "region" {
+  description = "The AWS region to deploy resources"
   type        = string
+  default     = "us-east-1"  # Change to your preferred region
 }
 
-variable "aws_secret_access_key" {
-  description = "AWS secret access key"
-  type        = string
-}
-
+# Define the AMI ID for the EC2 instances
 variable "ami_id" {
-  description = "The AMI ID for the Kubernetes master and worker nodes"
+  description = "AMI ID for the EC2 instances"
   type        = string
+  default     = "ami-0866a3c8686eaeeba"  # Replace with a valid AMI ID for your region
 }
 
+# Define the instance type for the master node
 variable "master_instance_type" {
-  description = "Instance type for the Kubernetes master"
+  description = "EC2 instance type for Kubernetes master"
   type        = string
-  default     = "t2.micro"  # Adjust the default as needed
+  default     = "t2.medium"  # Change based on your needs
 }
 
+# Define the instance type for the worker nodes
 variable "worker_instance_type" {
-  description = "Instance type for the Kubernetes workers"
+  description = "EC2 instance type for Kubernetes workers"
   type        = string
-  default     = "t2.micro"  # Adjust the default as needed
+  default     = "t2.micro"  # Change based on your needs
+}
+
+# Define the instance type for the monitoring server
+variable "monitoring_instance_type" {
+  description = "EC2 instance type for monitoring server"
+  type        = string
+  default     = "t2.small"  # Change based on your needs
+}
+
+# Define the number of worker instances to create
+variable "worker_count" {
+  description = "Number of Kubernetes worker instances to launch"
+  type        = number
+  default     = 2  # Change as needed
+}
+
+# Define tags to apply to the resources
+variable "tags" {
+  description = "Tags to apply to the resources"
+  type        = map(string)
+  default     = {
+    Name = "K8s-Instance"
+  }
 }
