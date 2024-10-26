298c351f4fb21ec4ec47f33ab4337cbbd609b496
Vijaya150 <vijayadarshini1503@gmail.com>
2024-10-26 11:03:15 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"  # or your desired region
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 resource "aws_instance" "k8s_master" {
